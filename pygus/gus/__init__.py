081d948a7f69cb74724f87cf6fc85196dddc69dc
jake <jake@lucidminds.ai>
2024-03-14 21:48:50 +0000

    Reverse the priority of the area function for x/y to lat/lng
    include export of pop. generation functions

--- pygus/gus/__init__.py
+++ pygus/gus/__init__.py
@@ -2,5 +2,6 @@ from .models import Urban, SiteConfig, WeatherConfig, ScenarioConfig
 from .agents import Tree
 from .allometrics import Species
 from .weather import WeatherSim
+from .population import tree_population_from_geojson, generate_population_features
 
 from .utilities import get_raster_data, latlng_array_to_xy, calculate_dataframe_area, filter_dataframe_to_bounding_box
